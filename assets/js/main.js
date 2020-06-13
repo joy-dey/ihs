7bd97a5f52f5fa9dbac9dad5529f94d5852a78bb
joy-dey <36854712+joy-dey@users.noreply.github.com>
2020-06-14 01:35:36 +0530

    Added Services section on this update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,6 @@ typewriter.typeString('Welcome to Imperial Health Care Services')
     .pauseFor(2500)
     .deleteAll()
     .typeString(`Tripura's largest Home Health Care Company`)
-    
-    .pauseFor(2500)
+
+.pauseFor(2500)
     .start();
