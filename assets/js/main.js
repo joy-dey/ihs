e668661e6bb7b87cfe13ef8c658262c1960797ad
joy-dey <36854712+joy-dey@users.noreply.github.com>
2020-07-25 21:51:26 +0530

    updated with other pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+document.getElementById('year').innerHTML = new Date().getFullYear();
+
+var scrollpos = window.scrollY;
+var header = document.getElementById("cta");
+
+function add_class_on_scroll() {
+    header.classList.add("d-block");
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("d-block");
+}
+
+window.addEventListener('scroll', function() {
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 15) {
+        add_class_on_scroll();
+    } else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
+
+
 let autotext = document.getElementById('bannertitle');
 
 var typewriter = new Typewriter(bannertitle, {
